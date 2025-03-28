66e04645b4722a5376c34095200d0bb6e24f4690
Daksh Shah <dakshudayshah@users.noreply.github.com>
2025-03-28 16:34:12 -0700

    fix usability issues

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -13,18 +13,11 @@ export const BottomNav = () => {
         <span>Home</span>
       </Link>
       
-      <Link to="/markets" className={`nav-item ${location.pathname === '/markets' ? 'active' : ''}`}>
+      <Link to="/create" className={`nav-item ${location.pathname === '/create' ? 'active' : ''}`}>
         <svg viewBox="0 0 24 24" width="24" height="24">
-          <path fill="currentColor" d="M3.5 18.49l6-6.01 4 4L22 6.92l-1.41-1.41-7.09 7.97-4-4L2 16.99z"/>
+          <path fill="currentColor" d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/>
         </svg>
-        <span>Markets</span>
-      </Link>
-
-      <Link to="/search" className={`nav-item ${location.pathname === '/search' ? 'active' : ''}`}>
-        <svg viewBox="0 0 24 24" width="24" height="24">
-          <path fill="currentColor" d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
-        </svg>
-        <span>Search</span>
+        <span>Create</span>
       </Link>
 
       <Link to="/profile" className={`nav-item ${location.pathname === '/profile' ? 'active' : ''}`}>
